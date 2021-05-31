91952504d94549ab9196f966b828039e11dfed0b
JungMin Ahn <jungminahn.minsone@gmail.com>
2021-06-01 00:34:23 +0900

    Dependencies에 SwiftPackageManager 일단 추가함.

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,10 +6,18 @@ let dependencies = Dependencies(
             .github(path: "FLEXTool/FLEX", requirement: .branch("master"))
         ],
         options: [
-           .useXCFrameworks,
-           .noUseBinaries
+            .useXCFrameworks,
+            .noUseBinaries
         ]
     ),
-    swiftPackageManager: nil, // work in progress, pass `nil`
+    swiftPackageManager: .swiftPackageManager(
+        [
+            .package(url: "https://github.com/Alamofire/Alamofire.git", .branch("master")),
+            .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+            .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .branch("master")),
+            .package(url: "https://github.com/layoutBox/FlexLayout.git", .branch("master")),
+            .local(path: .relativeToRoot("Projects/Modules/ResourcePackage")),
+        ]
+    ),
     platforms: [.iOS]
 )
