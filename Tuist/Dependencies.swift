379c4882102a227b82677ea79572319670aa0023
JungMin Ahn <jungminahn.minsone@gmail.com>
2021-06-01 23:20:55 +0900

    FLEX가 DYLIB_INSTALL_NAME_BASE가 지정이 되어 있지 않아 /Library/Frameworks 경로에서 FLEX를 불러와 문제 발생하여 수정한 버전을 사용

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,7 +3,8 @@ import ProjectDescription
 let dependencies = Dependencies(
     carthage: .carthage(
         [
-            .github(path: "FLEXTool/FLEX", requirement: .branch("master"))
+            .github(path: "minsone-opensource-fork/FLEX",
+                    requirement: .revision("2ad3092f4b9e31fc7294e45f3ee241324e17f0b3"))
         ],
         options: [
             .useXCFrameworks,
