7717e4230671c10e70ebf3f30d07125c8bfb61d2
JungMin Ahn <jungminahn.minsone@gmail.com>
2021-10-13 23:37:00 +0900

    의존성 그래프 스크립트 수정

---
+++ script/generate_tuist_graph.sh
@@ -0,0 +1,15 @@
+echo "Example, Testing이 없는 의존성 그래프를 만듭니다."
+tuist graph -t -f dot
+sed -i '' '/Example/d; /Testing/d' graph.dot
+dot -Tpng graph.dot -o Asset/graph.png
+dot -Tpdf graph.dot -o Asset/graph.pdf
+rm graph.dot
+
+echo "Example, Testing, 외부 라이브러리가 없는 의존성 그래프를 만듭니다."
+tuist graph -d -t -f dot
+sed -i '' '/Example/d; /Testing/d' graph.dot
+dot -Tpng graph.dot -o Asset/graph_without_external.png
+dot -Tpdf graph.dot -o Asset/graph_without_external.pdf
+rm graph.dot
+
+open Asset/graph_without_external.pdf
--- script/tuist_graph_without_testing_example_tests.sh
+++
@@ -1,6 +0,0 @@
-tuist graph -t -f dot
-sed -i '' '/Example/d; /Testing/d' graph.dot
-dot -Tpng graph.dot -o Asset/graph.png
-dot -Tpdf graph.dot -o Asset/graph.pdf
-rm graph.dot
-open Asset/graph.pdf
